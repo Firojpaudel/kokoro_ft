b41b330386ef8d68d587bb054c471dac0ff6d5fe
Firoj-Wise <firoj.paudel@wiseyak.com>
2026-07-17 11:54:50 +0545

    Allow Gradio to auto-select free port

--- gradio_app.py
+++ gradio_app.py
@@ -153,4 +153,4 @@ with gr.Blocks() as app:
     )
 
 if __name__ == "__main__":
-    app.queue().launch(server_name="0.0.0.0", server_port=7861, share=True)
+    app.queue().launch(server_name="0.0.0.0", share=True)
