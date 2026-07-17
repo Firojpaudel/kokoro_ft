597ddbb6688bdde8cf2200e17e8a3795280e0bba
Firoj-Wise <firoj.paudel@wiseyak.com>
2026-07-17 11:27:06 +0545

    Enable sharing in Gradio app

--- gradio_app.py
+++ gradio_app.py
@@ -90,4 +90,4 @@ with gr.Blocks() as app:
     )
 
 if __name__ == "__main__":
-    app.queue().launch(server_name="0.0.0.0", server_port=7861)
+    app.queue().launch(server_name="0.0.0.0", server_port=7861, share=True)
